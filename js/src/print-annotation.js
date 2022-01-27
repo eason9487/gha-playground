49a6e133b9033aac3b6bd6f04e1c7bed9411297c
Eason <eason.su.tw@gmail.com>
2022-01-27 17:52:48 +0800

    Update print-annotation.js

--- js/src/print-annotation.js
+++ js/src/print-annotation.js
@@ -4,3 +4,5 @@ console.log( `::error file=js/src/app.js,line=9,endLine=9,col=7,endColumn=8::[no
 console.log( `::error file=js/src/app.js,line=9,endLine=9,col=9,endColumn=11::[eqeqeq] Expected '===' and instead saw '=='.` );
 console.log( `::error file=js/src/app.js,line=10,endLine=10,col=3,endColumn=14::[no-console] Unexpected console statement.` );
 console.log( `::warning file=/Users/eason/wp-local-docker-sites/wp-test/wordpress/wp-content/plugins/google-listings-and-ads/js/src/app.js,line=18,endLine=18,col=5,endColumn=14::[react-hooks/exhaustive-deps] React Hook useMemo has a missing dependency: 'newValue'. Either include it or remove the dependency array.` );
+
+process.exit( 1 );
